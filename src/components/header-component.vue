90ae0354496ab5810e1a60058e87e97f28a6808f
Miroslav Lincovsci <lincovsci02@gmail.com>
2023-09-08 12:58:55 +0300

    improve header code

--- src/components/header-component.vue
+++ src/components/header-component.vue
@@ -81,13 +81,40 @@ const themeStore = useThemeStore();
       </button>
 
       <button
-        v-if="themeStore.currentTheme === 'white'"
         @click="themeStore.toggleColorTheme()"
-        class="dark-theme-toggler-button shadow-3xl flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg bg-zinc-600 duration-300 hover:bg-zinc-700"
+        :class="[
+          themeStore.currentTheme === 'light' ? 'bg-zinc-600' : 'bg-orange-300',
+          themeStore.currentTheme === 'light'
+            ? 'hover:bg-zinc-700'
+            : 'hover:bg-orange-400',
+          'shadow-3xl',
+          'flex',
+          'h-12',
+          'w-12',
+          'cursor-pointer',
+          'items-center',
+          'justify-center',
+          'rounded-lg',
+          'duration-300',
+        ]"
+        :aria-label="
+          themeStore.currentTheme === 'light'
+            ? 'Switch to Dark Theme'
+            : 'Switch to Light Theme'
+        "
       >
         <svg
-          class="h-8 w-8 fill-yellow-50 duration-500 dark:fill-zinc-900"
-          viewBox="0 0 24 24"
+          :class="[
+            'h-8',
+            'h-8',
+            'w-8',
+            'fill-yellow-50',
+            'duration-500',
+            themeStore.currentTheme === 'dark' && 'dark:fill-zinc-900',
+          ]"
+          :viewBox="
+            themeStore.currentTheme === 'dark' ? '-5.5 0 32 32' : '0 0 24 24'
+          "
           xmlns="http://www.w3.org/2000/svg"
         >
           <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
@@ -98,34 +125,14 @@ const themeStore = useThemeStore();
           ></g>
           <g id="SVGRepo_iconCarrier">
             <path
+              v-if="themeStore.currentTheme === 'light'"
               d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z"
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
             ></path>
-          </g>
-        </svg>
-      </button>
-      <button
-        v-if="themeStore.currentTheme === 'dark'"
-        @click="themeStore.toggleColorTheme()"
-        class="shadow-3xl flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg bg-orange-300 duration-300 hover:bg-orange-400"
-      >
-        <svg
-          class="h-8 w-8 fill-yellow-50 duration-500 dark:fill-zinc-900"
-          viewBox="-5.5 0 32 32"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-          <g
-            id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></g>
-          <g id="SVGRepo_iconCarrier">
-            <title>light</title>
             <path
+              v-if="themeStore.currentTheme === 'dark'"
               d="M11.875 6v2.469c0 0.844-0.375 1.25-1.156 1.25s-1.156-0.406-1.156-1.25v-2.469c0-0.813 0.375-1.219 1.156-1.219s1.156 0.406 1.156 1.219zM14.219 9.25l1.438-2.031c0.469-0.625 1.063-0.75 1.656-0.313s0.656 1 0.188 1.688l-1.438 2c-0.469 0.688-1.031 0.75-1.656 0.313-0.594-0.438-0.656-0.969-0.188-1.656zM5.781 7.25l1.469 2c0.469 0.688 0.406 1.219-0.219 1.656-0.594 0.469-1.156 0.375-1.625-0.313l-1.469-2c-0.469-0.688-0.406-1.219 0.219-1.656 0.594-0.469 1.156-0.375 1.625 0.313zM10.719 11.125c2.688 0 4.875 2.188 4.875 4.875 0 2.656-2.188 4.813-4.875 4.813s-4.875-2.156-4.875-4.813c0-2.688 2.188-4.875 4.875-4.875zM1.594 11.813l2.375 0.75c0.781 0.25 1.063 0.719 0.813 1.469-0.219 0.75-0.75 0.969-1.563 0.719l-2.313-0.75c-0.781-0.25-1.063-0.75-0.844-1.5 0.25-0.719 0.75-0.938 1.531-0.688zM17.5 12.563l2.344-0.75c0.813-0.25 1.313-0.031 1.531 0.688 0.25 0.75-0.031 1.25-0.844 1.469l-2.313 0.781c-0.781 0.25-1.281 0.031-1.531-0.719-0.219-0.75 0.031-1.219 0.813-1.469zM10.719 18.688c1.5 0 2.719-1.219 2.719-2.688 0-1.5-1.219-2.719-2.719-2.719s-2.688 1.219-2.688 2.719c0 1.469 1.188 2.688 2.688 2.688zM0.906 17.969l2.344-0.75c0.781-0.25 1.313-0.063 1.531 0.688 0.25 0.75-0.031 1.219-0.813 1.469l-2.375 0.781c-0.781 0.25-1.281 0.031-1.531-0.719-0.219-0.75 0.063-1.219 0.844-1.469zM18.219 17.219l2.344 0.75c0.781 0.25 1.063 0.719 0.813 1.469-0.219 0.75-0.719 0.969-1.531 0.719l-2.344-0.781c-0.813-0.25-1.031-0.719-0.813-1.469 0.25-0.75 0.75-0.938 1.531-0.688zM3.938 23.344l1.469-1.969c0.469-0.688 1.031-0.781 1.625-0.313 0.625 0.438 0.688 0.969 0.219 1.656l-1.469 1.969c-0.469 0.688-1.031 0.813-1.656 0.375-0.594-0.438-0.656-1.031-0.188-1.719zM16.063 21.375l1.438 1.969c0.469 0.688 0.406 1.281-0.188 1.719s-1.188 0.281-1.656-0.344l-1.438-2c-0.469-0.688-0.406-1.219 0.188-1.656 0.625-0.438 1.188-0.375 1.656 0.313zM11.875 23.469v2.469c0 0.844-0.375 1.25-1.156 1.25s-1.156-0.406-1.156-1.25v-2.469c0-0.844 0.375-1.25 1.156-1.25s1.156 0.406 1.156 1.25z"
             ></path>
           </g>
