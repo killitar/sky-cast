def436b9810a618fbdc024e1370f46c8b036698a
Miroslav Lincovsci <lincovsci02@gmail.com>
2023-09-08 12:54:51 +0300

    improve theme switching, add TS types

--- src/store/themeStore.ts
+++ src/store/themeStore.ts
@@ -1,39 +1,45 @@
-import { ref, onMounted } from 'vue';
-import { defineStore } from 'pinia';
+import { ref, onMounted, onUnmounted, Ref } from 'vue';
+import { defineStore, Store } from 'pinia';
+
+interface ThemeStore extends Store {
+  currentTheme: Ref<string>;
+  toggleColorTheme: () => void;
+}
 
 export const useThemeStore = defineStore('theme', () => {
-  const theme = window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'white';
-
-  const currentTheme = ref<string>(theme);
+  const currentTheme = ref<string>('light');
 
   const toggleColorTheme = () => {
-    if (currentTheme.value === 'white') {
-      currentTheme.value = 'dark';
-      document.documentElement.classList.add('dark');
-      document.documentElement.classList.remove('white');
-    } else {
-      currentTheme.value = 'white';
-      document.documentElement.classList.add('white');
-      document.documentElement.classList.remove('dark');
-    }
+    const newTheme: string = currentTheme.value === 'light' ? 'dark' : 'light';
+    document.documentElement.classList.remove(currentTheme.value);
+    document.documentElement.classList.add(newTheme);
+    currentTheme.value = newTheme;
   };
 
-  const checkTheme = () => {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
+  const detectSystemTheme = () => {
+    const prefersDarkMode = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    if (prefersDarkMode !== (currentTheme.value === 'dark')) {
+      toggleColorTheme();
     }
   };
 
   onMounted(() => {
-    checkTheme();
+    detectSystemTheme();
+
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const listener = () => detectSystemTheme();
+
+    mediaQuery.addEventListener('change', listener);
+
+    onUnmounted(() => {
+      mediaQuery.removeEventListener('change', listener);
+    });
   });
 
   return {
     currentTheme,
     toggleColorTheme,
-  };
+  } as ThemeStore;
 });
