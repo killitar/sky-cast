ca4bba07c6922974e17f4b14068a620c7389a62c
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-03-09 11:07:17 +0300

    fix(views):remove unused html block

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -103,17 +103,5 @@ const roundedFellsLike = computed(() => {
         <HourlyForecast />
       </div>
     </div>
-    <div v-else>
-      <div class="mt-32 flex place-items-center items-center justify-center">
-        <div
-          class="inline-block h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-zinc-800 motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-stone-100"
-          role="status"
-        >
-          <span
-            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-          ></span>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
